0b4f96761b50255400de6d35dfac18a32b2e2c46
abinesheswaran <47799838+abinesheswaran@users.noreply.github.com>
2021-10-23 23:12:31 +0530

    test commit-1

--- server.js
+++ server.js
@@ -14,15 +14,15 @@ dotenv.config({
 const app = require('./app');
 
 
-//DB path
-const DB = process.env.DATABASE.replace(`<password>`, process.env.DATABASE_PASSWORD);
+// //DB path
+// const DB = process.env.DATABASE.replace(`<password>`, process.env.DATABASE_PASSWORD);
 
-mongoose
-    .connect(DB, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => console.log('DB connection successful!'));
+// mongoose
+//     .connect(DB, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     })
+//     .then(() => console.log('DB connection successful!'));
 
 
 const port = process.env.PORT || 3000;
